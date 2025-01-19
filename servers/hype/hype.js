a41b3be4631bcdfd835738869a952c94c60b2e85
Rekonstrukcja <Botelmer781@gmail.com>
2025-01-19 21:26:09 +0100

    Add files via upload

---
+++ servers/hype/hype.js
@@ -0,0 +1,143 @@
+let currentImageIndex = 0;
+let currentImages = [];
+
+const categories = {
+    lokalizacje: [
+        { name: "DarkShop", images: ["darkshop1.png", "darkshop2.png", "darkshop3.png"], description: "DarkShop z nielegal itemami", download: "" },
+        { name: "Domy", images: ["dom1.png", "dom2.png"], description: "Zlecenia na łupanie chaup", download: "" },
+        { name: "Grupa6", images: ["grupa1.png", "grupa2.png", "grupa3.png"], description: "Zlecenia na łupanie grupe6", download: "" },
+ 
+ 
+        //         { name: "", images: [".png"], description: "", download: "" },
+    ],
+//    narko: [
+//        { name: "", images: [""], description: "", download: "" },
+//        { name: "", images: [""], description: "", download: "" },
+ 
+//    ]
+        //         { name: "", images: [".png"], description: "", download: "" },
+};
+
+function showImage(imageIndex) {
+    const map = document.getElementById('map');
+    const desc = document.getElementById('image-description');
+    
+    if (currentImages.length > 0) {
+        map.src = `img/${currentImages[imageIndex]}`;
+        map.style.display = 'block'; 
+        map.style.objectFit = 'cover'; 
+        map.style.width = '600px'; 
+        map.style.height = '600px'; 
+        document.getElementById('arrow-container').style.display = 'flex';
+    } else {
+        map.style.display = 'none';
+        document.getElementById('arrow-container').style.display = 'none';
+    }
+
+    updateArrows();
+}
+
+function updateArrows() {
+    const leftArrow = document.getElementById('left-arrow');
+    const rightArrow = document.getElementById('right-arrow');
+
+    if (currentImages.length <= 1) {
+        leftArrow.style.display = 'none';
+        rightArrow.style.display = 'none';
+    } else {
+        leftArrow.style.display = 'block';
+        rightArrow.style.display = 'block';
+    }
+}
+
+function showNextImage() {
+    if (currentImages.length > 1) {
+        currentImageIndex = (currentImageIndex + 1) % currentImages.length;
+        showImage(currentImageIndex);
+    }
+}
+
+function showPreviousImage() {
+    if (currentImages.length > 1) {
+        currentImageIndex = (currentImageIndex - 1 + currentImages.length) % currentImages.length;
+        showImage(currentImageIndex);
+    }
+}
+
+function fitMapToContainer() {
+    const map = document.getElementById('map');
+    const container = document.getElementById('map-container');
+    
+    const containerAspectRatio = container.offsetWidth / container.offsetHeight;
+    const mapAspectRatio = map.naturalWidth / map.naturalHeight;
+    
+    if (mapAspectRatio > containerAspectRatio) {
+        map.style.width = 'auto';
+        map.style.height = '100%';
+    } else {
+        map.style.width = '100%';
+        map.style.height = 'auto';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    fitMapToContainer();
+    window.addEventListener('resize', fitMapToContainer);
+    
+    const menuItems = document.querySelectorAll('.menu-item');
+    menuItems.forEach(item => {
+        item.addEventListener('click', function() {
+            const category = this.getAttribute('data-category');
+            const submenuContainer = document.getElementById('submenu-container');
+            submenuContainer.classList.toggle('show'); 
+
+            if (submenuContainer.classList.contains('show')) {
+                submenuContainer.classList.remove('hide'); 
+                submenuContainer.innerHTML = ''; 
+
+                categories[category].forEach(subcategory => {
+                    const div = document.createElement('div');
+                    div.className = 'submenu-item';
+                    div.textContent = subcategory.name;
+                    div.setAttribute('data-images', JSON.stringify(subcategory.images));
+                    div.setAttribute('data-description', subcategory.description);
+                    div.setAttribute('data-download', subcategory.download);
+                    submenuContainer.appendChild(div);
+
+                    div.addEventListener('click', function() {
+                        currentImages = JSON.parse(this.getAttribute('data-images'));
+                        const description = this.getAttribute('data-description') || "Brak";
+                        const download = this.getAttribute('data-download') || "#";
+                        currentImageIndex = 0;
+                        showImage(currentImageIndex);
+                        document.getElementById('description-text').textContent = description;
+                        const downloadText = document.getElementById('download-text');
+                        downloadText.textContent = download === "#" ? "Brak" : "Download";
+                        downloadText.href = download;
+                    });
+                });
+            } else {
+                submenuContainer.classList.add('hide'); 
+                setTimeout(() => submenuContainer.innerHTML = '', 500);
+            }
+        });
+    });
+
+    document.getElementById('left-arrow').addEventListener('click', function() {
+        showPreviousImage();
+    });
+
+    document.getElementById('right-arrow').addEventListener('click', function() {
+        showNextImage();
+    });
+
+    // ZMIEN API ZA KAZDYM RAZEM API SERWERA LICZBA GRACZY
+    fetch('https://servers-frontend.fivem.net/api/servers/single/r8b5y8')
+        .then(response => response.json())
+        .then(data => {
+            const playerCount = data.Data.clients;
+            const maxPlayers = data.Data.sv_maxclients;
+            document.getElementById('player-count').textContent = `[${playerCount}/${maxPlayers}]`;
+        })
+        .catch(error => console.error('Błąd pobierania liczby graczy:', error));
+});
